250b5c4cde8f1902db2504db9afe67ea2d9967c5
belicfr <96206878+belicfr@users.noreply.github.com>
2025-04-03 04:02:21 -0400

    feat: implement home button

--- src/game/gui/navbar/NavigationBar.tsx
+++ src/game/gui/navbar/NavigationBar.tsx
@@ -1,14 +1,17 @@
 import {FC} from "react";
 import "./NavigationBar.css";
 
-type Props = object;
+type Props = {
+  onHomeClick: () => void,
+};
 
 export const NavigationBar: FC<Props> = props => {
   return (
     <>
       <div className="navigation-bar__container">
         <div className="navigation-bar">
-        {/*  */}
+          <div className="navigation-bar__icon-home"
+               onClick={props.onHomeClick}></div>
         </div>
       </div>
     </>
