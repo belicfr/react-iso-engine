8a2968d01bc18cc6cb4e4dafe0ae10685f559bda
belicfr <96206878+belicfr@users.noreply.github.com>
2025-04-03 05:22:45 -0400

    feat: create welcome window on game load

--- src/game/GameView.tsx
+++ src/game/GameView.tsx
@@ -2,11 +2,16 @@ import {FC, useState} from "react";
 import {HotelView} from "./hotel-view/HotelView.tsx";
 import {NavigationBar} from "./gui/navbar/NavigationBar.tsx";
 import {TopOptions} from "./gui/top-options/TopOptions.tsx";
+import {ModalWindow} from "./gui/windows/ModalWindow.tsx";
 
 type Props = object;
 
 export const GameView: FC<Props> = props => {
   const [isHotelViewOpened, setIsHotelViewOpened] = useState(true);
+
+  const [ isWelcomeWindowOpened, setIsWelcomeWindowOpened ] = useState(true);
+
+  const onHomeClick = () => setIsHotelViewOpened(!isHotelViewOpened);
 
   return (
     <>
@@ -15,8 +20,19 @@ export const GameView: FC<Props> = props => {
       {isHotelViewOpened &&
         <HotelView />}
 
-      <NavigationBar onHomeClick={
-        () => setIsHotelViewOpened(!isHotelViewOpened)} />
+      {isWelcomeWindowOpened &&
+          <ModalWindow title="Welcome"
+                       width="auto"
+                       height="auto"
+                       onClose={() => setIsWelcomeWindowOpened(false)}>
+
+              <p style={{margin: "20px"}}>
+                  Welcome to this Demo.
+                  This engine is under development.
+              </p>
+          </ModalWindow>}
+
+      <NavigationBar onHomeClick={onHomeClick} />
     </>
   );
 };
