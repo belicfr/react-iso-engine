38c6be916e618738caaefbcf4ddef7dfdce3eb76
belicfr <96206878+belicfr@users.noreply.github.com>
2025-04-04 02:02:44 -0400

    feat: create StaffTools component

---
+++ src/game/gui/staff-tools/StaffTools.tsx
@@ -0,0 +1,58 @@
+import {FC, useState} from "react";
+import {SmallButton} from "../buttons/SmallButton.tsx";
+import "./StaffTools.css";
+import {ModToolsWindow} from "./windows/ModToolsWindow.tsx";
+
+type Props = {
+  canOpenModTools: boolean,
+  canBeInvisible: boolean,
+  canUseEffect: boolean,
+};
+
+export const StaffTools: FC<Props> = props => {
+  const [ isModToolsOpened, setIsModToolsOpened ] = useState(false);
+  const [ isInvisible, setIsInvisible ] = useState(false);
+  const [ isUsingEffect, setIsUsingEffect ] = useState(false);
+
+  const toggleModTools = () => setIsModToolsOpened(!isModToolsOpened);
+  const toggleInvisible = () => setIsInvisible(!isInvisible);
+  const toggleEffect = () => setIsUsingEffect(!isUsingEffect);
+
+  return (
+    <>
+      <div className="staff-buttons">
+        {props.canOpenModTools &&
+            <SmallButton
+                color="secondary"
+                onClick={toggleModTools}
+            >
+
+                MOD Tools
+            </SmallButton>}
+
+        {props.canBeInvisible &&
+            <SmallButton
+                color={isInvisible ? "danger" : "success"}
+                onClick={toggleInvisible}
+            >
+
+                {isInvisible ? "Disable" : "Enable"}
+                &nbsp;Invisible
+            </SmallButton>}
+
+        {props.canUseEffect &&
+            <SmallButton
+                color={isUsingEffect ? "danger" : "success"}
+                onClick={toggleEffect}
+            >
+
+              {isUsingEffect ? "Disable" : "Enable"}
+                &nbsp;Staff Effect
+            </SmallButton>}
+      </div>
+
+      {isModToolsOpened &&
+          <ModToolsWindow onClose={toggleModTools} />}
+    </>
+  );
+};
