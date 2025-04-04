3b412a65e9d1e283d2e23c57c636de9dbc2e7ab0
belicfr <96206878+belicfr@users.noreply.github.com>
2025-04-04 03:37:35 -0400

    feat: implement "bring-to-front" window system

--- src/game/gui/windows/Window.tsx
+++ src/game/gui/windows/Window.tsx
@@ -1,15 +1,33 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import {WindowBaseProps, WindowInterface} from "./WindowInterface.tsx";
 import {Rnd} from "react-rnd";
+import {useZIndex} from "./WindowContext.tsx";
 
 type Props = WindowBaseProps & {
 
 };
 
 export const Window: FC<Props> = props => {
+  const { bringToFront } = useZIndex();
+  const [ zIndex, setZIndex ] = useState(0);
+
+  useEffect(() => {
+    setZIndex(bringToFront());
+  }, []);
+
+  const handleFocus = () => {
+    setZIndex(bringToFront());
+  };
+
   return (
     <>
-      <Rnd style={{zIndex: 99}} enableResizing={false} dragHandleClassName="window-interface__header">
+      <Rnd
+        style={{zIndex}}
+        enableResizing={false}
+        dragHandleClassName="window-interface__header"
+        onMouseDown={handleFocus}
+      >
+
         <div className="window__container">
           <WindowInterface title={props.title}
                            width={props.width}
