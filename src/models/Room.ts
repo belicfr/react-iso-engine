01929025ddf341d7334dd1fde9309f3d4ec0a5a4
belicfr <96206878+belicfr@users.noreply.github.com>
2025-04-04 01:13:40 -0400

    feat: add players count and limit properties to Room model

--- src/models/Room.ts
+++ src/models/Room.ts
@@ -5,13 +5,53 @@ export default class Room {
   name: string;
   owner: User;
   tags: RoomTags;
+  playersLimit: number;
 
-  constructor(id: number, name: string, owner: User, tags: RoomTags) {
+  playersCount: number;
+
+  constructor(
+    id: number,
+    name: string,
+    owner: User,
+    tags: RoomTags,
+    playersLimit: number,
+    playersCount: number = 0) {
+
     this.id = id;
     this.name = name;
     this.owner = owner;
     this.tags = tags;
+    this.playersLimit = playersLimit;
+
+    this.playersCount = playersCount;
+  }
+
+  getPopulationLevel(): RoomPopulation {
+    const max: number = this.playersLimit;
+    const lowLimit: number = max / 3;
+    const mediumLimit: number = lowLimit * 2;
+
+    if (this.playersCount > 0
+               && this.playersCount <= lowLimit) {
+
+      return RoomPopulation.LOW;
+    } else if (this.playersCount > lowLimit
+               && this.playersCount <= mediumLimit) {
+
+      return RoomPopulation.MEDIUM;
+    } else if (this.playersCount > mediumLimit) {
+      return RoomPopulation.HIGH;
+    }
+
+    return RoomPopulation.NONE;
   }
 };
 
 type RoomTags = [string?, string?];
+
+export enum RoomPopulation {
+  NONE = "none",
+  LOW = "low",
+  MEDIUM = "medium",
+  HIGH = "high",
+}
