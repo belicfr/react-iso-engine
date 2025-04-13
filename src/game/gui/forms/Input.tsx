462edcb00f747da05279a7652d793c47228d6776
belicfr <96206878+belicfr@users.noreply.github.com>
2025-04-13 01:14:37 -0400

    feat: implement ref attribute on Input component

--- src/game/gui/forms/Input.tsx
+++ src/game/gui/forms/Input.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {forwardRef} from "react";
 import "./Control.css";
 import {ControlProps} from "./ControlProps.ts";
 
@@ -11,36 +11,40 @@ type Props = ControlProps & {
   maxLength?: number,
 };
 
-export const Input: FC<Props> = (
-  {
-    label, type, placeholder,
-    min, max, minLength,
-    maxLength, value,
+export const Input = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      label, type, placeholder,
+      min, max, minLength,
+      maxLength, value,
 
-    onChange, onBlur,
+      onChange, onBlur,
+    },
+    ref
+  ) => {
+    return (
+      <>
+        <label>
+          <span className="label">
+            {label}
+          </span>
+
+          <input
+            ref={ref}
+            className="control"
+            type={type ?? "text"}
+            placeholder={placeholder}
+            min={min}
+            max={max}
+            minLength={minLength}
+            maxLength={maxLength}
+            value={value}
+
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+        </label>
+      </>
+    );
   }
-) => {
-  return (
-    <>
-      <label>
-        <span className="label">
-          {label}
-        </span>
-
-        <input
-          className="control"
-          type={type ?? "text"}
-          placeholder={placeholder}
-          min={min}
-          max={max}
-          minLength={minLength}
-          maxLength={maxLength}
-          value={value}
-
-          onChange={onChange}
-          onBlur={onBlur}
-        />
-      </label>
-    </>
-  );
-};
+);
