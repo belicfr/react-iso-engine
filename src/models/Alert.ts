63d30983299692f8d7e1e5f928e263fe69de04ee
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-08 18:34:23 -0400

    feat: create Alert model

---
+++ src/models/Alert.ts
@@ -0,0 +1,14 @@
+export default class Alert {
+  id: number;
+
+  title: string;
+  content: string;
+
+  constructor(id: number, title: string, content: string) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+};
+
+export type AlertAction = (alert: Alert) => void;
