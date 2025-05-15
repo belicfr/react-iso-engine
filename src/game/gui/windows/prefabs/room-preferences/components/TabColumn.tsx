e5be27316218707818ff94dd2c65d75dc0f3d4c3
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-15 16:07:12 -0400

    refactor: set TabColumn component's title optional

--- src/game/gui/windows/prefabs/room-preferences/components/TabColumn.tsx
+++ src/game/gui/windows/prefabs/room-preferences/components/TabColumn.tsx
@@ -2,17 +2,18 @@ import {FC, ReactNode} from "react";
 import "./TabColumn.css";
 
 type Props = {
-  title: string,
+  title?: string,
   children: ReactNode,
 };
 
 export const TabColumn: FC<Props> = ({title, children}) => {
   return (
     <>
-      <section className="room-preferences__tab-column">
-        <h6 className="col-title">
-          {title}
-        </h6>
+      <section className="tab-column">
+        {title &&
+          <h6 className="col-title">
+            {title}
+          </h6>}
 
         {children}
       </section>
