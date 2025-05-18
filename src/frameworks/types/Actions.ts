af2d9dba6d0ef5ba9983734c75baecc260d927f0
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-18 02:13:37 -0400

    refactor: replace Promotino stub model action by DTO one

--- src/frameworks/types/Actions.ts
+++ src/frameworks/types/Actions.ts
@@ -6,6 +6,7 @@ import {PublicUserDto} from "../../models/dto/public/PublicUserDto.ts";
 import {RestrictedUserDto} from "../../models/dto/restricted/RestrictedUserDto.ts";
 import {PublicAlertDto} from "../../models/dto/public/PublicAlertDto.ts";
 import {PublicRoomTemplateDto} from "../../models/dto/public/PublicRoomTemplateDto.ts";
+import {PublicPromotionDto} from "../../models/dto/public/PublicPromotionDto.ts";
 
 export type Action = () => void;
 
@@ -17,8 +18,8 @@ export type RoomTemplateAction = (template: PublicRoomTemplateDto) => void;
 
 export type GroupAction = (group: Group) => void;
 
-export type PromotionArticleAction = (promotionArticle: PromotionArticle) => void;
-
 export type NotificationAction = (notification: Notification) => void;
 
 export type AlertAction = (alert: PublicAlertDto) => void;
+
+export type PromotionAction = (promotion: PublicPromotionDto) => void;
