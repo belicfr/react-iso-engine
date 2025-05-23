56ea46cd2a083de4ac31acffb73f316298396b87
belicfr <96206878+belicfr@users.noreply.github.com>
2025-05-23 03:40:21 -0400

    feat: add `PublicUserDto.position` attribute

--- src/models/dto/public/PublicUserDto.ts
+++ src/models/dto/public/PublicUserDto.ts
@@ -1,6 +1,9 @@
+import {Coord2D} from "../../../game/room-view/engine/precepts/Coord2D.ts";
+
 export type PublicUserDto = {
   id: string,
   userName: string,
   normalizedUserName: string,
   crowns?: number,
+  position: Coord2D,
 };
