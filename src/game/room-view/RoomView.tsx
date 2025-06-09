133000a8f1f3e73b394f56941edc5f1308890345
belicfr <96206878+belicfr@users.noreply.github.com>
2025-06-09 15:38:23 -0400

    feat: implement new hasCameraMoved state var

--- src/game/room-view/RoomView.tsx
+++ src/game/room-view/RoomView.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {FC, useRef, useState} from "react";
 import "./RoomView.css";
 import {Application as GameEnvironment, extend} from "@pixi/react";
 import {Container, Graphics, Sprite} from 'pixi.js';
@@ -6,8 +6,8 @@ import {RoomFloor} from "./components/room/RoomFloor.tsx";
 import {Camera} from "./components/camera/Camera.ts";
 import {UserAction} from "../../frameworks/types/Actions.ts";
 import RoomTemplate from "../../models/RoomTemplate.ts";
-import {PublicRoomDto} from "../../models/dto/public/PublicRoomDto.ts";
 import {PublicUserDto} from "../../models/dto/public/PublicUserDto.ts";
+import {PublicRenderRoomDto} from "../../models/dto/public/PublicRenderRoomDto.ts";
 
 extend({
   Container,
@@ -16,14 +16,17 @@ extend({
 });
 
 type Props = {
-  room: PublicRoomDto,
+  room: PublicRenderRoomDto,
   players: PublicUserDto[],
 
   onPlayerFocus: UserAction,
 };
 
 export const RoomView: FC<Props> = ({room, players, onPlayerFocus}) => {
-  const [ isCameraMoving, setIsCameraMoving ] = useState<boolean>(false);
+  const isCameraMoving = useRef<boolean>(false);
+  const hasCameraMoved = useRef<boolean>(false);
+
+  console.log(room.template);
 
   return (
     <GameEnvironment
@@ -34,16 +37,23 @@ export const RoomView: FC<Props> = ({room, players, onPlayerFocus}) => {
     >
 
       <RoomFloor
+        room={room}
         players={players}
         tilesPositions={RoomTemplate.generate(room.template)}
+
         isCameraMoving={isCameraMoving}
+        hasCameraMoved={hasCameraMoved}
 
         onPlayerFocus={onPlayerFocus}
       />
 
       <Camera
-        onCameraStartMove={() => setIsCameraMoving(true)}
-        onCameraStopMove={() => setIsCameraMoving(false)}
+        onCameraPointerDown={() => hasCameraMoved.current = false}
+        onCameraStartMove={() => {
+          hasCameraMoved.current = true;
+          isCameraMoving.current = true;
+        }}
+        onCameraStopMove={() => isCameraMoving.current = false}
       />
     </GameEnvironment>
   );
